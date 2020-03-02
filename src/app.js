cb42e8014fa31d9da06d6713041e57b23d051edd
Jonathan Wong <Jonwyp@users.noreply.github.com>
2020-03-02 17:03:36 +0800

    [Jon/WM] #7 Add testing for route and app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
+const articleRouter = require("./routes/article.route");
 
 app.use(express.json());
+
+app.use("/articles", articleRouter);
 
 app.get("/", (req, res) => {
   res.json({
     0: "GET /",
     1: "POST /articles"
   });
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500);
+  if (err.statusCode) {
+    res.send({ error: err.message });
+  } else {
+    res.send({ error: "Internal server error." });
+  }
 });
 
 module.exports = app;
