192fecabacce83d5bdf8c77716206d6853c88793
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2020-03-02 17:10:12 +0800

    Remove next in 500 code handler

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   });
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   res.status(err.statusCode || 500);
   if (err.statusCode) {
     res.send({ error: err.message });
