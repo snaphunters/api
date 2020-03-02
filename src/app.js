4d82a4bdb53ac07e67c5cb5fabd5af85e4b0458e
Jonathan Wong <Jonwyp@users.noreply.github.com>
2020-03-02 15:20:37 +0800

    [Jon/WM] #7 Add article model and route

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    0: "GET /",
+    1: "POST /articles"
+  });
+});
+
 module.exports = app;
